c7c2ed869d6c51ebc2184836445cd0405a47c89f
Rangy Abines <rangyabines11@gmail.com>
2016-08-09 12:11:33 -0400

    updated files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -26,7 +26,7 @@ router.get('/burgers', function (req, res) {
 });
 
 router.post('/burgers/create', function (req, res) {
-	burger.create(['name', 'sleepy'], [req.body.burger_name, req.body.devoured], function () {
+	burger.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], function () {
 		res.redirect('/burgers');
 	});
 });
